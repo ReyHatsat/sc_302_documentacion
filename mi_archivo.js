803d17ad4c0903eda406ea9a8e0f6d40cc443907
Rey_hatsat <78886551+ReyHatsat@users.noreply.github.com>
2025-01-27 20:46:02 -0600

    cambio dos del ejercicio de semana 3
    este es el segundo cambio hecho desde github de escritorio

--- mi_archivo.js
+++ mi_archivo.js
@@ -1,2 +1,4 @@
 console.log("hola mundo");
 // esto es un comentario para la semana 3 del curso de documentacion de software
+console.log("Daniel Tinajero Quesada");
+// este es otro comentario y este cambio se va a subir desde github desktop
